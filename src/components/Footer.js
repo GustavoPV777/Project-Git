7c8d61482def32a91180ad41eb3e8349bc4393ce
Gustavo Pacchi <h.gustavo.pv777@gmail.com>
2017-08-18 16:56:49 -0400

    first commit 3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Footer = () => ( < div className = "footer" >
+const Footer = () => ( < div className = "fooer" >
     <
     h > mostrar: < /h> <
     a href = "" > Todos < /a> {''} <
